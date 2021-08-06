ec9e5824a5da1126fd4039e70662bfa4a15b897f
CrawLeyYou <kaanbenkansoy@gmail.com>
2021-08-06 13:43:03 +0300

    update package/Encoder add README and License

--- README.md
+++ README.md
@@ -1,19 +1,33 @@
 # NodeJS-Encoder-Decoder
 
-This repo include a files that encode ur input and also has decoder for encoded string
+[![Size](https://img.shields.io/github/languages/code-size/CrawLeyYou/NodeJS-Encoder-Decoder)]()
+[![License](https://img.shields.io/github/license/CrawLeyYou/NodeJS-Encoder-Decoder)]()
+[![Stars](https://img.shields.io/github/stars/CrawLeyYou/NodeJS-Encoder-Decoder)]()
+[![Release](https://img.shields.io/github/v/release/CrawLeyYou/NodeJS-Encoder-Decoder)]()
 
-# Encoder Example
+This repository contains a file that encodes your input and also has the decoder for the decode encoded output.
 
-node Encode.js "Haha decode pew pew3169"
+# Installation
+Firstly install [NodeJS](https://nodejs.org/en/) with [npm](https://nodejs.org/en/knowledge/getting-started/npm/what-is-npm/) and after installation type: 
+``` 
+npm i
+```
+to CMD or anything else you using.
 
-Output: Your Encrypted String is: tOEvLzcGV3IHBldzMxNjk=SGFoYXAxIDSBkZWNvZGUg/z/21/5/5
-# Decoder Example 
+# Example of Usage
+```
+node Encoder.js "Haha decode pew pew3169"
+
+Your Encrypted String is: tOEvLzcGV3IHBldzMxNjk=SGFoYXAxIDSBkZWNvZGUg/z/21/5/5
+```
+```
 node Decoder.js "tOEvLzcGV3IHBldzMxNjk=SGFoYXAxIDSBkZWNvZGUg/z/21/5/5"
 
-Output: 
 Your Encrypted input is: tOEvLzcGV3IHBldzMxNjk=SGFoYXAxIDSBkZWNvZGUg/z/21/5/5
-
 Your Decrypted string is: Haha decode pew pew3169
+```
+# Comments
+If you use repetitive sentences / special letters, the program may fail because "/" will occur in between, I haven't fixed it yet because I don't want to spend all my day on this. And sometimes the encrypted string semi-readable in base64 decoding in 1.0.0 If you want the resulting string to be fully encrypted, you can check from any online base64 decoder.
 
-# Warning
-If you use repetitive sentences / special letters, the program may fail because "/" will occur in between, I haven't fixed it yet because I don't want to spend all my day on this lol 
+# License
+This project is licensed under [MIT](https://opensource.org/licenses/MIT) license.
---
+++ src/Encoder.js
@@ -0,0 +1,68 @@
+const base64lib = require("base-64");
+const utf8lib = require('utf8');
+const chars = require('ramda')
+
+var input = process.argv[2];
+
+var UTF8 = utf8lib.encode(input);
+var Base64 = base64lib.encode(UTF8);
+var Chars = chars.length(Base64)
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(6);
+    max = Math.floor(Chars);
+    return Math.floor(Math.random() * (max - min) + min); 
+}
+
+function RandomLenght(min, max) {
+    min = Math.ceil(5);
+    max = Math.floor(10);
+    return Math.floor(Math.random() * (max - min) + min); 
+}
+
+
+function StringMaker() {
+    var CharsFirst1 = Base64
+    var CharDivided = Chars/2
+    var stringone = CharsFirst1.split("", CharDivided)
+    var stringtwo = stringone.toString();
+    var stringout = stringtwo.split(",").join("");
+    var Second = Base64.split(stringout).toString().split(",").join("");
+   var FinalString = Second + stringout
+   return FinalString
+}
+
+function EncryptedString(String) {
+
+var randomNumber = getRandomInt()
+var randomlenght = RandomLenght()
+var randomstring = makeid(randomlenght)
+var randomChar = makeid(1)
+var randomstringonstartup = (makeid(RandomLenght(5)))
+
+var randomstringonstartuplenght = chars.length(randomstringonstartup)
+var randomstringlenght = chars.length(randomstring)
+
+var stringone = String.split("", randomNumber)
+var stringtwo = stringone.toString().split(",").join("");
+var stringout1 = randomstringonstartup + randomChar + stringtwo + randomstring
+var stringout2 = String.split(stringtwo).toString().split(",").join("");
+var stringout = stringout1 + stringout2 + "/" + randomChar +"/" + randomNumber +"/"+ randomstringlenght + "/" + randomstringonstartuplenght
+
+return stringout
+}
+
+var EncString = EncryptedString(StringMaker())
+
+console.info("Your Encrypted String is: " + EncString)
