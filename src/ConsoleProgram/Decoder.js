28645b4383f4687960a66f78c0089acb8bb8bbe4
CrawLeyYou <60201017+CrawLeyYou@users.noreply.github.com>
2021-08-17 11:59:31 +0300

    Delete test things again i didn't see

--- src/ConsoleProgram/Decoder.js
+++ src/ConsoleProgram/Decoder.js
@@ -20,7 +20,6 @@ var SFS6 = SFS5.split("", RNSL).toString().split(",").join("")
 var SFS7 = SFS5.split(SFS6).toString().split(",").join("")
 var SFS8 = SFS4 + SFS7
 var SFS9 = chars.length(SFS8)
-var test = SFS8.length
 var SFS10 = SFS9/2
 var SFS11 = SFS8.split("", SFS10).toString().split(",").join("");
 var SFS12 = SFS8.split(SFS11).toString().split(",").join("");
